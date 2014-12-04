3db3423c9ff0049365d677dad90af3878dd346b8
antti <4ntti.matikainen@gmail.com>
2014-12-04 12:12:55 +0200

    modified:   client/app/scripts/controllers/kuvagalleria.js 	modified:   client/app/views/kuvagalleria.html 	modified:   readme.md 	modified:   server/dist/index.html 	new file:   server/dist/scripts/scripts.263e727e.js 	deleted:    server/dist/scripts/scripts.8afa2136.js 	modified:   server/dist/views/kuvagalleria.html

--- client/app/scripts/controllers/kuvagalleria.js
+++ client/app/scripts/controllers/kuvagalleria.js
@@ -35,12 +35,14 @@
    			// 	"foldername_1": ["foo.png", "daa.png"],
    			// 	"foldername_2": ["abc.png"], "fii.png"]
    			// }
-   			var slides1 = [];
+   			var slides = [];
+   			var slideHeaders = [];
    			var img;
    			// iterate properties(folders) of object "galleria_1", galleria_2...
    			for (var folder in data.images){
    				console.log("folder: " + folder);
    				var fileNames = data.images[folder];
+   				slideHeaders.push(folder);
    				console.log(fileNames);
    				// iterate array of fileNames
    				for (var i in fileNames){
@@ -50,10 +52,28 @@
    					 	"image": "/images/galleria/" + folder + "/" + fileName
    					 }
    					console.log(img);
-   					slides1.push(img);
+   					slides.push(img);   					
    				}
    			}
-   			$scope.slides1 = slides1;
+
+   			console.log(slides);
+   			console.log(slideHeaders);
+   			$scope.slideItems = [
+   					{ 
+   						header: "galleria_1",
+   					  	images: [ 
+   							{image: '/images/galleria/galleria_1/1.jpg'},
+   							{image: '/images/galleria/galleria_1/2.jpg'}
+						]
+					},
+					{ 
+   						header: "2",
+   					  	images: [ 
+   							{ image: '/images/galleria/2/2.jpg' },
+   							{ image: '/images/galleria/2/3.jpg'}
+						]
+					}
+   			]
    		}).
    		error(function(data){
    			console.log(data);
@@ -63,7 +83,7 @@
    // var promise = prepareSlides();
    // promise.then(function(images){
    // 		console.log('galleria response :' + images.msg);
-   //  	$scope.slides1 = images.images_1;	
+   //  	$scope.slides = images.images_1;	
    //  	$scope.slides2 = images.images_2;	
    // }, function(reason){
    // 		console.log(reason);
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
 # Follow these instructions
 http://start.jcolemorrison.com/building-an-angular-and-express-app-part-1/
 
-# Quick start
-run server -> cd /server && npm test
+# Quick start | run server
+ cd /server && npm test
+
+#Build to dist
+cd /client && grunt build --force
+
+#run server production mode
+cd /server && npm start
